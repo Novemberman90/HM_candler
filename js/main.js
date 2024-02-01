9e702258e42790a18d6d4b79f8f86d26fc863327
Alexey <alexledovit@gmail.com>
2024-02-01 05:20:22 +0200

    add: swiper code

--- js/main.js
+++ js/main.js
@@ -4,3 +4,33 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', ()=>{
     menuMobile.classList.toggle('menu--open');
 });
+
+/* slide feedback */
+const swiperOne = new Swiper('.feedback__slider', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+/* slide certificate */
+  const swiperTwo = new Swiper('.certificates__slider', {
+    loop: true,
+    slidesPerView: 3,
+      spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+          },
+        480: {
+          slidesPerView: 2,
+        },
+        360: {
+            slidesPerView: 1,
+          }
+      },
+  });
